c84890b0f869e15037b8bf4f72252b0fdebf269c
mattzsinha <maitrayee.k10@gmail.com>
2025-03-16 16:16:01 +0530

    Update script.js

--- Week-I Task/script.js
+++ Week-I Task/script.js
@@ -17,3 +17,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Sticky Header with Scroll Effect
+window.addEventListener("scroll", function() {
+    const header = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
